5017ae342f5a019c6704ef5886cd9ed50af6d2d3
Ahmed Abdelazim <toshiba20.ahmed@gmail.com>
2018-11-13 18:42:51 +0200

    added some initial functions

--- functions.php
+++ functions.php
@@ -1,6 +1,13 @@
 <?php
 function get_game($id){
-
-
+    $aggregated_rating=$response[aggregated_rating];
 }
+function get_gener($id){}
+function get_platforms($id){}
+function get_release_dates($id){}
+function get_screenshote($id){}
+function get_artwork($id){} //cover
+function get_videos($id){}
+function get_websites($id){} // like twitter or instagram
+function get_aggregated_rating($response){}
 ?>
