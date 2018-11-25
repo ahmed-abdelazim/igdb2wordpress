d2371c633a8bc5e3bd516402f930b7fdec1700d0
ahmed-abdelazim <34234821+ahmed-abdelazim@users.noreply.github.com>
2018-11-25 12:47:46 +0200

    changed platforms file to get all playstation games ID's

--- platforms.php
+++ platforms.php
@@ -2,15 +2,24 @@
 header('Content-Type: application/json');
 include("config.php");
 include("functions.php");
-$i = 1;
+/* $i = 1;
 $pquery = '';
 while ($i<401){
 
     $pquery .= ','.$i;
     $i++;
 }
-$platforms = get_json_response ('game_engines','1'.$pquery.'?fields=name,id');
+$platforms = get_json_response ('game_engines','1'.$pquery.'?fields=name,id'); */
+$output = '[';
+$ps_platforms = array('48','165','7','8','9','38','45','46');
+//https://api-endpoint.igdb.com/platforms/48?fields=games
+foreach ($ps_platforms as $pid){
+    $platforms = rtrim(substr(get_json_response ('platforms',$pid.'?fields=games'), 1),']').',';
+    $output .= $platforms;
+    unset($platforms);
+    
+}
 //print_r($platforms);
 //var_dump($platforms);
-echo $platforms;
+echo rtrim($output,',').']';
 ?>
